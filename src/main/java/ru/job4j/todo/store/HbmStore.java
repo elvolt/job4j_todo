0abe0aff7ca4f910c2da06a18d0d6c96be1f0377
elvolt <sidelevcs@gmail.com>
2021-08-15 18:33:38 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/store/HbmStore.java
+++ src/main/java/ru/job4j/todo/store/HbmStore.java
@@ -2,12 +2,14 @@ package ru.job4j.todo.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todo.model.Item;
 
 import java.util.Collection;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -23,34 +25,46 @@ public class HbmStore implements Store, AutoCloseable {
         return Lazy.INST;
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     @Override
     public Collection<Item> getAllItems() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Collection items = session.createQuery("from Item order by created").list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+        return tx(
+                session -> session.createQuery("from Item order by created").list()
+        );
     }
 
     @Override
     public Item saveItem(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return tx(
+                session -> {
+                    session.save(item);
+                    return item;
+                }
+        );
     }
 
     @Override
     public Item updateItem(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return tx(
+                session -> {
+                    session.update(item);
+                    return item;
+                }
+        );
     }
 
     @Override
